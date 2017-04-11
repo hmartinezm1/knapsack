1cba151e797bd5e3521ee4ab0e7b0d1eac78d382
“Howard <howard.martinez@upr.edu>
2017-04-10 22:32:13 -0400

    Greedy Implementation

--- src/edu/uprm/admg/oasis/crisp/knapsack/Main.java
+++ src/edu/uprm/admg/oasis/crisp/knapsack/Main.java
@@ -3,18 +3,18 @@ package edu.uprm.admg.oasis.crisp.knapsack;
 public class Main {
 
     public static void main(String[] args) {
-        // Implementation with Dinamic Programming
-        //int values[] = new int[]{60, 100, 120};
-        //int weights[] = new int[]{10, 20, 30};
-        //int  W = 50;
-        //int n = values.length;
-        //System.out.println("The max value you can get is: "+Knapsack.knapSack(W, weights, values, n));
+        int values[] = new int[]{60, 100, 120};
+        int weights[] = new int[]{10, 20, 30};
+        int  W = 50;
+        int n = values.length;
+        System.out.println("Dinamic Programming - 0/1 Knapsack");
+        System.out.println("Maximum value in Knapsack = "+Knapsack.knapSack(W, weights, values, n));
 
-        // Implementation with Greedy
-        int W = 50;
+        //int W = 50;
         Knapsack test = new Knapsack();
         Knapsack arr[] = {new Knapsack(20, 100), new Knapsack(30, 120), new Knapsack(10, 60)};
-        int n = arr.length;
-        System.out.println("The max value you can get is: "+test.fractionalKnapsack(W, arr, n));
+        int N  = arr.length;
+        System.out.println("\n\nGreedy Approach - Fractional Knapsack");
+        System.out.println("Maximun value in Knapsack = "+test.fractionalKnapsack(W, arr, N));
     }
 }
