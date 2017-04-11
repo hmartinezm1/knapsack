3b2704a0f456b27303a6e76c1b7e5e70aaeb0ad6
“Howard <howard.martinez@upr.edu>
2017-04-10 22:03:27 -0400

    Greedy Implementation

--- src/edu/uprm/admg/oasis/crisp/knapsack/Knapsack.java
+++ src/edu/uprm/admg/oasis/crisp/knapsack/Knapsack.java
@@ -1,5 +1,7 @@
 package edu.uprm.admg.oasis.crisp.knapsack;
 
+import java.util.Arrays;
+
 /**
  * Created by howardmartinez
  * Author: Howard A. Martinez Meza
@@ -8,12 +10,68 @@ package edu.uprm.admg.oasis.crisp.knapsack;
  * Date: 08/04/17
  * Description:
  */
-public class Knapsack {
+public class Knapsack implements Comparable<Knapsack>{
 
-    static int max(int a, int b) { return (a > b)? a : b; }
+    private int weight;
+    private int value;
+    private double density;
 
-    // Returns the maximum value that can be put in a knapsack of capacity W
-    static int knapSack(int W, int weights[], int values[], int n) {
+    public Knapsack(){
+        weight = 0;
+        value = 0;
+        density = 0;
+    }
+
+    public Knapsack(int weight, int value){
+        if (weight == 0){
+            this.weight = 0;
+            this.density = 0;
+        }else{
+            this.weight = weight;
+            this.value = value;
+            this.density = (double)value/weight;
+        }
+    }
+
+    // Greedy function to solve problem
+    public double fractionalKnapsack(int W, Knapsack arr[], int n) {
+        //    sorting Item on basis of ration
+        Arrays.sort(arr);
+
+    for (int i = 0; i < n; i++) {
+        if (i == 0)
+            System.out.println("V\t\tW\t\tr");
+        System.out.println(arr[i].value+"\t\t"+arr[i].weight+"\t\t"+((double)arr[i].value / arr[i].weight));
+    }
+
+        int curWeight = 0;  // Current weight in knapsack
+        double finalvalue = 0.0; // Result (value in Knapsack)
+
+        // Looping through all Items
+        for (int i = 0; i < n; i++) {
+            // If adding Item won't overflow, add it completely
+            if (curWeight + arr[i].weight <= W) {
+                curWeight += arr[i].weight;
+                finalvalue += arr[i].value;
+            }
+            // If we can't add current Item, add fractional part of it
+            else {
+                int remain = W - curWeight;
+                finalvalue += arr[i].value * ((double) remain / arr[i].weight);
+                break;
+            }
+        }
+
+        // Returning final value
+        return finalvalue;
+    }
+
+
+    // Dinamic Programming
+    public static int max(int a, int b) { return (a > b)? a : b; }
+
+    // Returns the maximum value that can be put in a knapsack of capacity W --- DINAMIC PROGRAMMING
+    public static int knapSack(int W, int weights[], int values[], int n) {
         int i, w;
         int K[][] = new int[n+1][W+1];
 
@@ -29,5 +87,16 @@ public class Knapsack {
             }
         }
         return K[n][W];
+    }
+
+
+    @Override
+    public int compareTo(Knapsack o) {
+        if(o.density - this.density == 0)
+            return 0;
+        else if(o.density - this.density > 0)
+            return 1;
+        else
+            return -1;
     }
 }
--- src/edu/uprm/admg/oasis/crisp/knapsack/Main.java
+++ src/edu/uprm/admg/oasis/crisp/knapsack/Main.java
@@ -3,11 +3,18 @@ package edu.uprm.admg.oasis.crisp.knapsack;
 public class Main {
 
     public static void main(String[] args) {
-        Knapsack k = new Knapsack();
-        int values[] = new int[]{60, 100, 120};
-        int weights[] = new int[]{10, 20, 30};
-        int  W = 50;
-        int n = values.length;
-        System.out.println("The max value you can get is: "+k.knapSack(W, weights, values, n));
+        // Implementation with Dinamic Programming
+        //int values[] = new int[]{60, 100, 120};
+        //int weights[] = new int[]{10, 20, 30};
+        //int  W = 50;
+        //int n = values.length;
+        //System.out.println("The max value you can get is: "+Knapsack.knapSack(W, weights, values, n));
+
+        // Implementation with Greedy
+        int W = 50;
+        Knapsack test = new Knapsack();
+        Knapsack arr[] = {new Knapsack(20, 100), new Knapsack(30, 120), new Knapsack(10, 60)};
+        int n = arr.length;
+        System.out.println("The max value you can get is: "+test.fractionalKnapsack(W, arr, n));
     }
 }
